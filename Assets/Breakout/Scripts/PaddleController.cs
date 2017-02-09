a8940ae2db2763c4ad46f0059a8e0d1c49c201dc
Joseph Bedsole <jbedsole@institute.local>
2017-02-08 19:19:21 -0600

    I made a simple Breakout Game!

---
+++ Assets/Breakout/Scripts/PaddleController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleController : MonoBehaviour {
+
+
+    public float speed = 100;
+
+    void Update() {
+        float x = Input.GetAxis("Horizontal");
+        transform.position += Vector3.right * speed * x * Time.deltaTime;
+    }
+}
