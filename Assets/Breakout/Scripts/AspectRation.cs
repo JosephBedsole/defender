e0257812baf4f7da06281dcebb2c7ed5c43a14c4
Joseph Bedsole <jbedsole@dmi-intertech.org>
2017-08-11 18:43:37 -0500

    Last commit

---
+++ Assets/Breakout/Scripts/AspectRation.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AspectRation : MonoBehaviour {
+
+    public float aspectRatio = 16f / 9f;
+
+    void Start()
+    {
+        SetAspectRatio();
+    }
+
+    public void SetAspectRatio()
+    {
+        Camera camera = GetComponent<Camera>();
+        Rect rect = new Rect(0, 0, 1, 1);
+
+        float screenAspect = (float)Screen.width / (float)Screen.height;
+        float scale = screenAspect / aspectRatio;
+
+        if (scale < 1.0f)
+        {
+            rect.height = scale;
+            rect.y = (1.0f - scale) / 2.0f;
+        }
+        else
+        {
+            scale = 1.0f / scale;
+            rect.width = scale;
+            rect.x = (1.0f - scale) / 2.0f;
+        }
+        camera.rect = rect;
+    }
+}
